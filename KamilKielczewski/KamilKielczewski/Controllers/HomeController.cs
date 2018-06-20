08a1d5ebaf60c5edc8422fbe11dc0ee2c69d893d
Unknown <kam.kielczewski@gmail.com>
2018-06-20 20:02:30 +0200

    Done2

---
+++ KamilKielczewski/KamilKielczewski/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KamilKielczewski.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Form()
+        {
+            return View("Form");
+        }
+
+        public ActionResult Language()
+        {
+            return View("Language");
+        }
+
+        public ActionResult Change(string Language)
+        {
+
+            if (Language != null)
+            {
+
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(Language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Language);
+            }
+
+            HttpCookie myCookie = new HttpCookie("Language");
+            myCookie.Value = Language;
+            myCookie.Expires = DateTime.Now.AddDays(1d);
+            Response.Cookies.Add(myCookie);
+
+            return View("Index");
+        }
+    }
+}
---
+++ KamilKielczewski/KamilKielczewski/obj/Release/Package/PackageTmp/Scripts/Form.js
@@ -0,0 +1,56 @@
+﻿function ValidateForm() {
+    ValidateFirstName();
+    ValidateLastName();
+    ValidatePassword();
+
+    var email = document.getElementById('Email');
+
+    if (ValidateEmail(email.value)) {
+        email.style.border = '1px solid #ced4da';
+    } else {
+        email.style.border = '1px solid red';
+    }
+}
+
+function ValidateFirstName() {
+    var name = document.getElementById('firstName');
+    if (name.value != "") {
+        name.style.border = '1px solid #ced4da';
+        return;
+    }
+    name.style.border = '1px solid red';
+}
+
+function ValidateLastName() {
+    var name = document.getElementById('lastName');
+    if (name.value != "") {
+        name.style.border = '1px solid #ced4da';
+        return;
+    }
+    name.style.border = '1px solid red';
+}
+
+function ValidateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function ValidatePassword() {
+    var password = document.getElementById('Password');
+    var confirmPassword = document.getElementById('confirmPassword');
+
+    if (password.value != confirmPassword.value || password.value == "" || confirmPassword == "") {
+        password.style.border = '1px solid red';
+        confirmPassword.style.border = '1px solid red';
+    } else {
+        if (password.value.match(/^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)) {
+            password.style.border = '1px solid #ced4da';
+            confirmPassword.style.border = '1px solid #ced4da';
+        }
+    }
+}
+
+function ClosePopup() {
+    popup = document.getElementById('popupx');
+    popup.parentNode.removeChild(popup);
+}
